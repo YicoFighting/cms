3a1467dee629d9eb1d05e504f2e4f62fb91667f5
XiaoZiYan <2604482363@qq.com>
2021-12-06 14:57:18 +0800

    fix(生产环境): api

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
   TIME_OUT = 10000
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = '/api'
+  BASE_URL = 'http://152.136.185.210:5000'
   TIME_OUT = 10001
 } else {
   BASE_URL = '/api'
